cb9e1152f4ba3bef68c15db4f826a5869f7fc25f
Kevin <kejoxz@gmail.com>
2025-07-08 03:03:10 +0800

    feat: add back to top

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -7,19 +7,20 @@ const route = useRoute()
 const { data: post } = await useAsyncData(() =>
   queryCollection('blog').path(`/blog/${route.params.slug}`).first()
 )
-
 useSeoMeta({
   title: post.value?.title,
   description: post.value?.description
 })
 
-console.log(post.value)
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
 </script>
 
 <template>
 
-  <main class="flex flex-col gap-2 w-full sm:max-w-2xl">
-    <aside />
+  <div class="flex flex-col gap-2 w-full sm:max-w-2xl">
     <div class="flex flex-col gap-4">
       <div class="flex flex-col gap-1">
         <h1 class="lg:text-4xl">{{ post.title }}</h1>
@@ -31,16 +32,24 @@ console.log(post.value)
           }) }}
           - {{ post.readingTime }} Min Read
         </p>
-
       </div>
       <div class="flex gap-2">
         <p v-for="tag in post.tags" :key="tag" class="bg-zinc-700 px-2 rounded-sm text-sm"> #{{ tag }}</p>
       </div>
       <hr>
     </div>
+
     <div v-if="post" class="prose prose-sm lg:prose-base p-0 dark:prose-invert max-w-none">
       <ContentRenderer :value="post" class="text-justify" />
     </div>
+
     <p v-else>Post not found</p>
-  </main>
+
+    <button class=" self-end" @click="scrollToTop">
+
+      <p class="flex text-xs items-center gap-2">
+        <LucideArrowUp class="w-4" />Back To Top
+      </p>
+    </button>
+  </div>
 </template>
