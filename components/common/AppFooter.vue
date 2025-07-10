3307f5ab2c8b2ea082a40da2549b6d7961065803
Kevin <kejoxz@gmail.com>
2025-07-11 01:16:14 +0800

    feat: add footer

---
+++ components/common/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="w-full py-6 text-center text-xs text-black-700  mt-16">
+    <p>&copy; {{ currentYear }} Kevin Jonathan. All rights reserved.</p>
+  </footer>
+</template>
+
+<script setup>
+const currentYear = new Date().getFullYear()
+</script>
--- layouts/home-layout.vue
+++ layouts/home-layout.vue
@@ -1,6 +1,7 @@
 <template>
-  <main class="flex flex-col justify-center items-center px-4  w-full lg:max-w-3xl mx-auto">
+  <main class="flex flex-col justify-center items-center px-4 min-h-screen  w-full lg:max-w-3xl mx-auto">
     <NavBar />
     <slot />
+    <AppFooter />
   </main>
 </template>
