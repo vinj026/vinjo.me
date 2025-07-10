fc0192c6c4f7230136a45a89df7d03c227f99c76
Kevin <kejoxz@gmail.com>
2025-07-11 04:39:36 +0800

    refactor(blog): adjust some spacing

--- components/sections/learning-journals/BlogContent.vue
+++ components/sections/learning-journals/BlogContent.vue
@@ -12,23 +12,24 @@ const { data: posts } = await useAsyncData(route.path, () => {
 <template>
   <div>
     <template v-if="posts.length > 0">
-      <div v-for="post in posts" :key="post.path" class="flex flex-col gap-4 w-full">
-        <div class="flex flex-col">
-          <p class="text-xs pt-2 sm:text-sm">{{ post.date }}</p>
-          <NuxtLink :to="post.path" class="flex items-start justify-between text-lg text-gray-100 font-semibold">
-            {{ post.title }}
-            <LucideChevronRight class="mt-1" />
-          </NuxtLink>
+      <div class="flex flex-col gap-4">
+        <div v-for="post in posts" :key="post.path" class="flex flex-col gap-4 w-full">
+          <div class="flex flex-col">
+            <p class="text-xs pt-2 sm:text-sm">{{ post.date }}</p>
+            <NuxtLink :to="post.path" class="flex items-start justify-between text-lg text-gray-100 font-semibold">
+              {{ post.title }}
+              <LucideChevronRight class="mt-1" />
+            </NuxtLink>
+          </div>
+          <div class="flex items-center gap-2">
+            <p v-for="tag in post.tags" :key="tag" class="bg-zinc-900 px-2 h-fit rounded-sm text-xs">
+              #{{ tag }}
+            </p>
+          </div>
+          <hr class="text-zinc-800">
         </div>
-        <div class="flex items-center gap-2">
-          <p v-for="tag in post.tags" :key="tag" class="bg-zinc-900 px-2 h-fit rounded-sm text-xs">
-            #{{ tag }}
-          </p>
-        </div>
-        <hr class="text-zinc-800 mt-2">
       </div>
     </template>
-
     <template v-else>
       <div class="text-center text-sm text-zinc-500 py-10 italic">
         No posts found. Stay tuned for updates!
