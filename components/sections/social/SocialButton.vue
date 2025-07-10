baaf25ac1239306cee91744dee13d12d5333681f
Kevin <kejoxz@gmail.com>
2025-07-10 09:09:08 +0800

    chore: adjust social media sections

---
+++ components/sections/social/SocialButton.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  title: String,
+  icon: Object,
+  username: String,
+  link: String
+})
+</script>
+<template>
+
+  <div class="relative w-fit">
+    <a
+:href="link" target="_blank" rel="noopener noreferrer"
+      class="relative inline-block rounded-md py-1 px-2 group overflow-hidden">
+      <!-- background fill -->
+      <span class="absolute inset-0 w-0 bg-black-800 transition-all duration-300 ease-out group-hover:w-full z-0" />
+
+      <!-- content -->
+      <div class="relative z-10 flex items-center gap-2 text-white group-hover:text-white">
+        <component :is="icon" class="w-5 h-5" />
+        <p>{{ username }}</p>
+      </div>
+    </a>
+  </div>
+
+</template>
---
+++ components/sections/social/SocialMedia.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { Github, Linkedin, FileText, Mail } from 'lucide-vue-next'
+
+const socialMedia = ref([
+  {
+    icon: Github,
+    title: 'Github',
+    username: '@vinj026',
+    link: 'https://github.com/vinj026',
+  },
+  {
+    icon: Linkedin,
+    title: 'LinkedIn',
+    username: 'Kevin Jonathan',
+    link: 'https://linkedin.com/in/kevinjonathan26',
+  },
+  {
+    icon: Mail,
+    title: 'Email',
+    username: 'vinjonathan@gmail.com',
+    link: 'mailto:vinj026@email.com',
+  },
+  // {
+  //   icon: FileText,
+  //   title: 'FileText',
+  //   username: '',
+  //   link: '',
+  // },
+])
+</script>
+
+<template>
+  <ContentContainer title="Let's Connect">
+    <div class="flex flex-col gap-2">
+      <SocialButton
+v-for="(item, i) in socialMedia" :key="i" :icon="item.icon" :title="item.title" :link="item.link"
+        :username="item.username" />
+    </div>
+  </ContentContainer>
+</template>
