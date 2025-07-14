7223f0a7ec5511e36950967c65911253c269fa18
Kevin <kejoxz@gmail.com>
2025-07-14 09:12:53 +0800

    style: change banner style

--- components/common/AppHeader.vue
+++ components/common/AppHeader.vue
@@ -1,14 +1,18 @@
 <template>
   <header class="w-full">
     <div class="flex flex-col w-full h-fit ">
-      <div class="relative">
-        <NuxtImg src="/images/banner1.png" alt="Banner"
-          class="h-[150px] w-full object-cover object-center rounded-b-2xl border-white-700" />
-      </div>
+      <ClientOnly>
+        <div class="relative size-48 w-full overflow-hidden rounded-lg border bg-background">
+          <FlickeringGrid
+            class="relative inset-0 z-0 [mask-image:radial-gradient(450px_circle_at_center,white,transparent)]"
+            :square-size="4" :grid-gap="6" color="#6a6a6a" :max-opacity="0.5" :flicker-chance="0.2" :width="800"
+            :height="800" />
+        </div>
+      </ClientOnly>
       <div class="flex flex-col justify-between p-2">
         <div class="relative">
           <NuxtImg src="/images/profilepic.png" alt="Profile Picture"
-            class="absolute bottom-0 left-4 translate-y-[30%] w-[120px] h-[120px] object-cover object-center rounded-full border-4 border-[#080808]" />
+            class="absolute bottom-0 left-4 translate-y-[30%] w-[120px] h-[120px] object-cover object-center rounded-full border-2 border" />
         </div>
       </div>
       <div class="h-[40px]" />
