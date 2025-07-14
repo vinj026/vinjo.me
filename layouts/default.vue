2e78bc2d389efa8087ddfcc333a453565d7d1732
Kevin <kejoxz@gmail.com>
2025-07-15 03:55:22 +0800

    fix(ui): add consistent height to navbar and adjust layout spacing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
-  <main class="flex flex-col items-center px-4 min-h-screen w-full lg:max-w-3xl mx-auto">
-    <AppNavbar />
-    <slot />
-    <div class="flex grow" />
-    <AppFooter />
+  <main class="flex flex-col min-h-screen items-center px-4 w-full lg:max-w-3xl mx-auto">
+    <AppNavbar class="min-h-[64px]" />
+    <div class="flex-1 w-full">
+      <slot />
+    </div>
+    <AppFooter class="min-h-[48px]" />
   </main>
 </template>
