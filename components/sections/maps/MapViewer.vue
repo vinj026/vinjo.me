23433b73620451cba0f819dd8075af75da4c2da7
Kevin <kejoxz@gmail.com>
2025-07-16 06:45:21 +0800

    style: change map height on mobile

--- components/sections/maps/MapViewer.vue
+++ components/sections/maps/MapViewer.vue
@@ -5,7 +5,7 @@ const center = [-1.23919, 116.85869] // Balikpapan
 
 <template>
 
-  <div class="relative h-64 md:h-full group ">
+  <div class="relative h-40 md:h-full group ">
     <div class="absolute z-10 left-3 bottom-3 flex items-center gap-1">
       <div class="p-2 bg-black-900 rounded-2xl border border-black-600">
         <LucideMapPin class="w-3 h-3 text-zinc-300" />
@@ -18,15 +18,16 @@ class="bg-black-900 text-xs text-zinc-300 font-medium px-3 py-1 rounded-xl borde
         Balikpapan, Indonesia
       </div>
     </div>
-
-    <LMap
+    <ClientOnly>
+      <LMap
 :zoom="5" :center="center" :use-global-leaflet="false"
-      :options="{ zoomControl: false, attributionControl: false, dragging: false, touchZoom: false }"
-      class="grayscale brightness-[0.45]  h-full min-h-full w-full rounded-2xl outline-none">
-      <LTileLayer
+        :options="{ zoomControl: false, attributionControl: false, dragging: false, touchZoom: false }"
+        class="grayscale brightness-[0.45]  h-full min-h-full w-full rounded-2xl outline-none">
+        <LTileLayer
 :url="'/api/maptile/{z}/{x}/{y}.webp'" layer-type="base"
-        :options="{ tileSize: 512, directRetina: true, zoomOffset: -1 }" />
-    </LMap>
+          :options="{ tileSize: 512, directRetina: true, zoomOffset: -1 }" />
+      </LMap>
+    </ClientOnly>
     <div className='absolute inset-0 flex items-center justify-center'>
       <span class="relative flex size-3"> <span
           class="absolute inline-flex h-full w-full animate-ping rounded-full bg-emerald-400 opacity-55" /> <span
