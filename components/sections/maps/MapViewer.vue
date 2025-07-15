97ea0ced9bc2351c1473e04b0be54499180f812b
Kevin <kejoxz@gmail.com>
2025-07-15 21:07:18 +0800

    fix: match tileSize

--- components/sections/maps/MapViewer.vue
+++ components/sections/maps/MapViewer.vue
@@ -22,7 +22,7 @@ const center = [-1.23919, 116.85869] // Balikpapan
     <LMap :zoom="10" :center="center" :min-zoom="0" :max-zoom="50" :scroll-wheel-zoom="false"
       :options="{ zoomControl: false, attributionControl: false, dragging: false, touchZoom: false }"
       class="grayscale brightness-[0.45]  h-full min-h-full w-full rounded-2xl outline-none">
-      <LTileLayer :options="{ tileSize: 256 }" :url="'/api/maptile/{z}/{x}/{y}.png'" />
+      <LTileLayer :detect-retina="true" :tile-size="256" :url="'/api/maptile/{z}/{x}/{y}.png'" />
     </LMap>
 
     <div className='absolute inset-0 flex items-center justify-center'>
